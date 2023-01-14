420a2391c7e110b9e41497e723111b360f51294f
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-01-14 02:32:22 -0500

    created arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(a, b) {
+    let result = a/b;
+    return result;
+}
+
+const square = (x) => {
+    let result = x * x;
+    return result;
+}
+
+const add = (a, b) => {
+    let result = a + b;
+    return result;
+}
